2aa0b66e27f5c726f98f73e476ccb74d69be735b
tenickajaalena <tenickajaalena@gmail.com>
2019-05-15 00:28:25 +0300

    Добавьте файлы проекта.

---
+++ tpzslaba4(1)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace tpzslaba4_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            char[] symbol = { ' ', ',', '.', '!', '?', ':', ';', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            using (StreamReader MyFile = new StreamReader("input.txt"))
+            {
+                // читання рядка з файлу, поки це можливо
+                while ((line = MyFile.ReadLine()) != null)
+                {
+                    Console.WriteLine("Строка символов с файла: ");
+                    Console.WriteLine(line); //друкуємо рядок
+                    string result = string.Concat(line.Split(symbol, StringSplitOptions.RemoveEmptyEntries).Select(word => word.Last()));
+                    Console.WriteLine("Искаемая строка: ");
+                    Console.WriteLine(result);//друкуємо результат
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
